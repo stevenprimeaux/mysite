0ef0f3c380a85026c6f67218721eda1fdb2125ec
sprimeaux-nolapublicschools <sprimeaux@nolapublicschools.com>
2020-08-18 09:16:11 -0500

    Remove secret key.

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!5j9=#up&%(5igg$&5palihe4nzeya#49o0+z2*^f20=omo#!8'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
